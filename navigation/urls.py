7c303b3c66dc9db579cb99cd3a56ce1f53bd160e
Alexey Davletshin <naylrush@gmail.com>
2020-05-13 16:58:26 +0300

    Fix and upgrade dirs and files selection

--- navigation/urls.py
+++ navigation/urls.py
@@ -5,7 +5,6 @@ from navigation import views
 
 
 urlpatterns = [
-    path('', views.base),
     re_path(r'^(?P<path>(([\w\s]+/)*))$', views.go),
     re_path(r'^(?P<path>(([\w\s]+/)*[\w\s.]+))$', views.open)
 ]
--- navigation/views.py
+++ navigation/views.py
@@ -6,32 +6,25 @@ import os
 BASE_DIR = os.getcwd()
 
 
-def base(request):
-    files = os.listdir(BASE_DIR)
-    return render(request, 'navigation/index.html', context={'files': files})
-
-
 def go(request, path: str):
-    if path.endswith('..'):
-        path = os.path.basename(path)
-    elif path.endswith('.'):
-        # do nothing
-        path = path[:len(path) - 1]
-
+    """
+    Opens directory by path and returns its own files and directories
+    """
+    # links '.' and '..' are included to django (I think)
     path = os.path.join(BASE_DIR, path)
     all_files = os.listdir(path)
-    print(all_files)
-    files = [f for f in all_files if os.path.isfile(os.path.join(path, f))]
-    dirs = set(all_files) ^ set(files)
-    print(dirs, files)
+    dirs = [d for d in all_files if os.path.isdir(os.path.join(path, d))]
+    files = set(all_files) ^ set(dirs)
     return render(request, 'navigation/index.html', context={'dirs': dirs,
                                                              'files': files,
                                                              'content': None})
 
 
 def open(request, path: str):
+    """
+    Opens text file in UTF-8
+    """
     fd = os.open(path, os.O_RDONLY)
     content = (os.read(fd, os.stat(path).st_size)).decode('utf-8')
-    print(content)
     os.close(fd)
     return render(request, 'navigation/index.html', context={'content': content})
